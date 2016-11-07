8ed085861ada9c6c33be45813cee6118a6aecb3c
daniel.klava@innovative.com.br <Innova#90032>
2016-11-07 19:19:07 -0200

    Update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -6,12 +7,16 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 //Database
+var pg = require('pg');
+var conString = 'postgres://postgres:admindb@localhost/nodejs_db'
+
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/nodetest2');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var skills = require('./routes/skills');
 
 var app = express();
 
@@ -35,6 +40,7 @@ app.use(function(req,res,next){
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/skills', skills);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+//Database
+var pg = require('pg');
+var conString = 'postgres://postgres:admindb@localhost/nodejs_db'
+
 /*
  * GET userlist. 
  */
@@ -12,35 +16,87 @@ router.get('/', function(req, res){
  * GET userlist. 
  */
 router.get('/userlist',function(req, res){
-	var db = req.db;
-	var collection = db.get('userlist');
-	collection.find({},{},function(e,docs){
-		res.json(docs);
-	})
+
+	//var collection = db.get('userlist');
+	//collection.find({},{},function(e,docs){
+	//	res.json(docs);
+	//})
+})
+
+/*
+ * GET USERS - Postgre
+ */
+router.get('/get', function(req, res, next){
+
+    const results = [];
+
+    pg.connect(conString, function(err, client, done){
+        if (err){
+            done();
+            console.log(err);
+            return res.status(500).json({success:false, data:err});
+        }
+        const query = client.query('SELECT * FROM users;');
+
+        query.on('row', (row)=> {
+            results.push(row);
+        })
+
+        query.on('end', () =>{
+            done();
+            return res.json(results);
+        });
+    });
 })
 
 /*
  * POST to adduser.
  */
-router.post('/adduser', function(req, res) {
-    var db = req.db;
-    var collection = db.get('userlist');
-    collection.insert(req.body, function(err, result){
-        res.send(
-            (err === null) ? { msg: '' } : { msg: err }
-        );
-    });
+router.post('/add', function(req, res, next) {
+    const results= [];
+
+    const data = req.body;
+
+    pg.connect(conString, function(err,client,done){
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({success: false, data: err});
+        }
+
+        const query = client.query('INSERT INTO users (username, name, email, location, age, gender) VALUES ($1, $2, $3, $4, $5, $6);'
+            , [data.username, data.name, data.email, data.location, data.age, data.gender]);
+
+        query.on("end", function (result) {          
+            client.end(); 
+            res.write('Success');
+            res.end();  
+        });
+    })
 });
 
 /*
  * DELETE to deleteuser.
  */
-router.delete('/deleteuser/:id', function(req, res) {
-    var db = req.db;
-    var collection = db.get('userlist');
-    var userToDelete = req.params.id;
-    collection.remove({ '_id' : userToDelete }, function(err) {
-        res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+router.delete('/delete/:user_id', function(req, res, next) {
+
+    var userId = req.params.user_id;
+
+    pg.connect(conString,function(err,client,done){
+        if (err){
+            done();
+            console.log(err);
+            return res.status(500).json({success: false, data: err});
+        }
+
+        const query = client.query('DELETE FROM users WHERE username = ($1);', [userId]);
+
+        query.on("end", function (result) {          
+            client.end(); 
+            res.write('Success');
+            res.end();  
+        });
     });
 });
 
